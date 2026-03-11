c965f781d8a8ef36939221fd4c146f010268c139
DSH <86291175+dousihao@users.noreply.github.com>
2026-03-11 20:26:48 +0800

    Modify output directory in cls_ft.sh
    Updated output directory path for evaluation script.

--- videomamba/scripts/cls_ft.sh
+++ videomamba/scripts/cls_ft.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# 激活 conda
-source /gaoxieping/miniconda3/etc/profile.d/conda.sh
-conda activate fcrmamba
-cd /gaoxieping/dsh/FCRMamba/videomamba/downstream/PolypDiagClassification
-
-python eval_finetune1.py \
+python eval_finetune.py \
     --data_path /gaoxieping/dsh/data/downstream/PolypDiag/ \
-    --output_dir /gaoxieping/dsh/FCRMamba/videomamba/out/Classification/
+    --output_dir ../out/Classification/
