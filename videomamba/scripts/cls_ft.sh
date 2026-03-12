b723d8b37cf272b226d25de5778b76405eed72f0
DSH <86291175+dousihao@users.noreply.github.com>
2026-03-12 20:05:54 +0800

    Update cls_ft.sh

--- videomamba/scripts/cls_ft.sh
+++ videomamba/scripts/cls_ft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd /gaoxieping/dsh/FPRL/videomamba/downstream/PolypDiagClassification
 python eval_finetune.py \
     --data_path /gaoxieping/dsh/data/downstream/PolypDiag/ \
     --output_dir ../out/Classification/
