43a143b1b7886d77a037484e86998b1c19cb32f8
MLMIP <67742308+MLMIP@users.noreply.github.com>
2026-03-12 21:10:23 +0800

    Update det_ft.sh

--- videomamba/scripts/det_ft.sh
+++ videomamba/scripts/det_ft.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#export PYTHONPATH="$(cd "$(dirname "$0")/.." && pwd):$PYTHONPATH"
-# 激活 conda
-source /gaoxieping/miniconda3/etc/profile.d/conda.sh
-conda activate fcrmamba
-cd /gaoxieping/dsh/FCRMamba/videomamba/downstream/STFT
+
+cd /gaoxieping/dsh/FPRL/videomamba/downstream/STFT
 export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
 
 torchrun \
@@ -11,4 +8,4 @@ torchrun \
     tools/train_net.py \
     --master_port=$((RANDOM + 10000)) \
     --config-file configs/STFT/kumc_R_50_STFT.yaml \
-    OUTPUT_DIR /gaoxieping/dsh/FCRMamba/videomamba/out/Detection/fcrmamba_small_b64_seqlen2withWeaker__withteacher_MIX7
+    OUTPUT_DIR /gaoxieping/dsh/FPRL/videomamba/out/Detection/fcrmamba_small_b64_seqlen2withWeaker__withteacher_MIX7
