3f74156d3f8f6c6ef60121056ae188da8f45860b
MLMIP <67742308+MLMIP@users.noreply.github.com>
2026-03-12 21:12:27 +0800

    Update rec_ft.sh

--- videomamba/scripts/rec_ft.sh
+++ videomamba/scripts/rec_ft.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 激活 conda
-source /gaoxieping/miniconda3/etc/profile.d/conda.sh
-conda activate fcrmamba
-cd /gaoxieping/dsh/FCRMamba/videomamba/downstream/SV-RCNet
+cd /gaoxieping/dsh/FPRL/videomamba/downstream/SV-RCNet
 
 python train_singlenet_phase_1fc.py
